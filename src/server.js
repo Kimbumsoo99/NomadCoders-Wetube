94d95f6a32edcd1065861556039f55c40f73628c
Kimbumsoo99 <show7441@naver.com>
2023-01-22 02:04:53 +0900

    express 기초 연습

--- src/server.js
+++ src/server.js
@@ -1,18 +1,41 @@
 import express from "express";
 
-const PORT=4000;
+const PORT = 4000;
 const app = express();
 
-const handleHome = (req, res) =>  {
-    return res.send("I still love you");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
-const handleLogin=(req,res) => {
-    return res.send("Login here");
-}
-app.get("/",handleHome);
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you mat continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 app.get("/login", handleLogin);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-
-app.listen(PORT,handleListening)
+app.listen(PORT, handleListening);
