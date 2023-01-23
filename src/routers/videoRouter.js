1997ce130160896f73b0d8c673191f574e34d0e8
Kimbumsoo99 <show7441@naver.com>
2023-01-23 11:44:04 +0900

    정규식 및 파라미터 연습

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { edit, see, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see); //url에 변수를 추가할 수 있게한다는 의미
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
