20f7119199dbc4315b5c2ca70ede58a287ae6393
Kimbumsoo99 <show7441@naver.com>
2023-02-01 11:26:32 +0900

    User Schema 생성 및 Join 페이지

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
+import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -12,7 +13,7 @@ app.set("view engine", "pug"); //view engine 설정
 app.set("views", process.cwd() + "/src/views"); //views 폴더 경로
 app.use(logger); //morgan
 app.use(express.urlencoded({ extended: true })); //express가 form의 value들을 이해할 수 있도록 함.
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
