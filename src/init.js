4ca5cf53cb82a7fb5ff503d5c6ee843523c591fc
Kimbumsoo99 <show7441@naver.com>
2023-02-05 01:05:49 +0900

    dotenv 설정

--- src/init.js
+++ src/init.js
@@ -1,10 +1,11 @@
+import "dotenv/config";
+//require("dotenv").config(); //dotenv를 사용하고싶으면 모든 파일에 require를 사용
 import "./db";
 import "./models/Video";
 import "./models/User";
 import app from "./server";
 
 const PORT = 4000;
-
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
