cf4ec9cf4af7ec92950689a5b0712ab3667eef6e
Kimbumsoo99 <show7441@naver.com>
2023-02-06 13:56:14 +0900

    Github 로그인

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -69,5 +69,21 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "85bb6c4271b9ddfde058",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  console.log(params);
+  const finalUrl = `${baseUrl}?${params}`;
+
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const logout = (req, res) => res.send("Logout User");
 export const see = (req, res) => res.send("See User");
