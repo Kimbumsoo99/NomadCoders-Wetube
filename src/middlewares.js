a179f4b0efa6d820c3d761f69947b8a7342994d4
Kimbumsoo99 <show7441@naver.com>
2023-02-07 04:33:05 +0900

    Edit Profile

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.user = req.session.user;
+  res.locals.user = req.session.user || {};
   next();
 };
