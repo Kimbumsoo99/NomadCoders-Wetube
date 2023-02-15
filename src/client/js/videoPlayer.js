a31b4cef786ab4607c848e630a12ae2831709867
Kimbumsoo99 <show7441@naver.com>
2023-02-16 04:26:52 +0900

    JS 연습 3

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,8 +4,11 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 /**비디오 재생 버튼 클릭 */
-const handlePlayClick = (e) => {
+const handlePlayClick = (event) => {
   // if 비디오 재생중이면? 재생
   // else 일시정지
   if (video.paused) {
@@ -17,7 +20,7 @@ const handlePlayClick = (e) => {
 };
 
 /**음소거 버튼 */
-const handleMuteClick = (e) => {
+const handleMuteClick = (event) => {
   // 음소거
   if (video.muted) {
     video.muted = false;
@@ -25,8 +28,23 @@ const handleMuteClick = (e) => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  volumeValue = value;
+  video.volume = value;
+  if (volumeValue === String(0)) {
+    video.muted = true;
+  } else if (video.muted) {
+    video.muted = false;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
