2bc92ae470b594224953d4a893499b17765e0548
Kimbumsoo99 <show7441@naver.com>
2023-02-17 01:08:40 +0900

    조회수 등록 API 추가1

---
+++ src/routers/apiRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerView } from "../controllers/videoController";
+
+const apiRouter = express.Router();
+
+//localhost:4000/api/videos/124124312/view
+apiRouter.post("/videos//:id([0-9a-f]{24})/view", registerView);
+
+export default apiRouter;
