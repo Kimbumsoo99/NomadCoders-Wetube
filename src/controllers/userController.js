e5fd3228e7dd7c63f079445f220402f6154b97f9
Kimbumsoo99 <show7441@naver.com>
2023-02-17 16:41:16 +0900

    ffmpeg/wasm

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -91,6 +91,7 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+
   const tokenRequest = await (
     await fetch(finalUrl, {
       method: "POST",
@@ -99,6 +100,7 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
+
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
     const apiUrl = "https://api.github.com";
