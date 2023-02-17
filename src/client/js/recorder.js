bcf5d7e83f5653885f84013574021a33047c853b
Kimbumsoo99 <show7441@naver.com>
2023-02-17 14:30:29 +0900

    녹화 영상 1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,42 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream = null;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done");
+    console.log(event);
+    console.log(event.data);
+  };
+  console.log(recorder);
+  recorder.start(); //MediaRecorder를 사용하기때문에 사용 가능
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: { width: 200, height: 150 },
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
