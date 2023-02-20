a1c7fcba6ee4216970fc6900761cd2f35f1f4003
Kimbumsoo99 <show7441@naver.com>
2023-02-21 06:30:35 +0900

    댓글 등록 실시간 처리

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,19 @@ import { async } from "regenerator-runtime";
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = `  ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -11,7 +24,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${id}/comment`, {
+  const { status } = await fetch(`/api/videos/${id}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json", //express에 우리는 string을 보내는것이아니라 json string이라는것을 알려줌
@@ -19,7 +32,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  }
 };
 // JSON.stringify는 json 객체를 {"text":"i like it","rating":"5"} 의 형태로 바꿔줌
 //backend에서는 이것을 JS object로 바꿔준다 express.json() 을 통해서
